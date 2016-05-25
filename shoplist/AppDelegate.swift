442b31b9f12fceffe7cf74ee31d9be5e8e942dd8
artyombobrovnikov <Admin@BobrovnikovMacbookAir.local>
2016-05-25 17:03:15 +0300

    UI changes

--- shoplist/AppDelegate.swift
+++ shoplist/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         UINavigationBar.appearance().barTintColor = UIColor(red: 240.0/255, green: 43.0/255, blue: 43.0/255.0, alpha: 1.0)
         UINavigationBar.appearance().tintColor = UIColor.whiteColor()
-        if let barFont = UIFont(name: "Noteworthy-Light", size: 24.0){
+        if let barFont = UIFont(name: "HelveticaNeue-Thin", size: 24.0){
             UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor(),
             NSFontAttributeName: barFont]
             
